b2f8cd0cb0c83944ba6e7fb1dc8ff5e407c415d3
seda-man <seda.manukyan@picsart.com>
2017-11-27 21:59:38 +0400

    Shitty code

---
+++ client.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 4096
+#define SERVER_DEFAULT_PORT 3000
+
+int main(int argc, char** argv)
+{
+	int socketFD;
+	struct sockaddr_in serverAddress;
+	char sendingLine[MAX_LINE_LENGTH], receivingLine[MAX_LINE_LENGTH];
+
+	if (argc < 2) {
+		perror("No IP or hostname found");
+		exit(1);
+	}
+	
+	socketFD = socket(AF_INET, SOCK_STREAM, 0);
+	if (socketFD < 0) {
+		perror("Problem in socket creation");
+		exit(1);
+	}
+
+	memset(&serverAddress, 0, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = inet_addr(argv[1]);
+	int port = SERVER_DEFAULT_PORT;
+	if (argc > 2) {
+		char* portNumber = argv[2];
+		port = atoi(portNumber);
+	}
+	serverAddress.sin_port = htons(port);
+
+	if (connect(socketFD,
+	(struct sockaddr *) &serverAddress, 
+	sizeof(serverAddress)) < 0) {
+		perror("Problem in connecting to the server");
+		exit(1);
+	}
+	
+	while (1) {
+		printf("Your message : ");
+		if (read(0, sendingLine, MAX_LINE_LENGTH) < 0) {
+			perror("Problem while reading from standard input");
+			exit(1);
+		}
+
+		if (send(socketFD, sendingLine, strlen(sendingLine), 0) < 0) {
+			perror("Problem while sending message.");
+			exit(1);
+		}
+
+		if (recv(socketFD, receivingLine, MAX_LINE_LENGTH, 0) < 0) {
+			perror("Problem while receiving message.");
+			exit(1);
+		}
+
+		if (strcmp(receivingLine, "Server is disconnected") == 0) {
+			break;
+		}
+
+		printf("\nServer reply %s", receivingLine);
+		memset(sendingLine, 0, MAX_LINE_LENGTH);
+		memset(receivingLine, 0, MAX_LINE_LENGTH);
+	}
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 4096
+#define DEFAULT_SERVER_PORT 3000
+
+int compare_strings(char string1[], char string2[], int receivedCharacters) 
+{
+	int i = 0, count = 0;
+	while (string1[i] == string2[i] && ++count < receivedCharacters) {
+		if (string1[i] == '\0' || string2 == '\0') {
+			break;
+		}
+		i++;
+	}
+	if (count == receivedCharacters) {
+		return 0;
+	} else if (string1[i] == '\0' && string2[i] == '\0') {
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
+int main(int argc, char** argv) 
+{
+	int listeningSocketFD, connSocketFD, n;
+	socklen_t clientLength;
+	char readingBuffer[MAX_LINE_LENGTH];
+	struct sockaddr_in clientAddress, serverAddress;
+
+	listeningSocketFD = socket(AF_INET, SOCK_STREAM, 0);
+
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	int port = DEFAULT_SERVER_PORT;
+	if (argc == 2) {
+		char* portNumber = argv[2];
+		port = atoi(portNumber);
+	}
+	serverAddress.sin_port = htons(port);
+
+	bind(listeningSocketFD, 
+	(struct sockaddr *) &serverAddress, 
+	sizeof(serverAddress));
+
+	listen(listeningSocketFD, 1);
+
+	printf("%s\n","Server is running. Waiting for connection...");
+	while(1) {
+		clientLength = sizeof(clientAddress);
+		connSocketFD = accept(listeningSocketFD,
+			(struct sockaddr *) &clientAddress,
+			&clientLength);
+		
+		if (connSocketFD < 0) {
+			perror("Receiving failed");
+			exit(1);
+		}
+
+		pid_t child_pid = fork();
+		if (child_pid == 0) {
+			while ((n = recv(connSocketFD, 
+					readingBuffer, 
+					MAX_LINE_LENGTH, 0)))  {
+				
+				if (compare_strings(readingBuffer, "bye", n)==0){ 
+					write(connSocketFD, 
+						"Server is disconnected", 
+						23);
+					break;
+				}
+				printf("%s\n", "Message received from buffer.");
+				printf("%s\n", readingBuffer);
+				send(connSocketFD, readingBuffer, n, 0);
+				memset(readingBuffer, 0, MAX_LINE_LENGTH);
+			}
+
+			if (n < 0) {
+				perror("Read error");
+				exit(1);
+			}
+			close(connSocketFD);
+			memset(readingBuffer, 0, MAX_LINE_LENGTH);
+		}
+
+		close(listeningSocketFD);
+	}
+	return 0;
+}
